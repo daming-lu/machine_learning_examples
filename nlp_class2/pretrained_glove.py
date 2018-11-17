c40633d273420937d7c10a53ea6209cd90394e08
daming-lu <daminglu@yahoo.com>
2018-11-16 19:30:34 -0800

    switch back to faster way

--- nlp_class2/pretrained_glove.py
+++ nlp_class2/pretrained_glove.py
@@ -28,29 +28,6 @@ dist, metric = dist2, 'cosine'
 
 
 ## more intuitive
-def find_analogies(w1, w2, w3):
-  for w in (w1, w2, w3):
-    if w not in word2vec:
-      print("%s not in dictionary" % w)
-      return
-
-  king = word2vec[w1]
-  man = word2vec[w2]
-  woman = word2vec[w3]
-  v0 = king - man + woman
-
-  min_dist = float('inf')
-  best_word = ''
-  for word, v1 in iteritems(word2vec):
-    if word not in (w1, w2, w3):
-      d = dist(v0, v1)
-      if d < min_dist:
-        min_dist = d
-        best_word = word
-  print(w1, "-", w2, "=", best_word, "-", w3)
-
-
-## faster
 # def find_analogies(w1, w2, w3):
 #   for w in (w1, w2, w3):
 #     if w not in word2vec:
@@ -62,16 +39,39 @@ def find_analogies(w1, w2, w3):
 #   woman = word2vec[w3]
 #   v0 = king - man + woman
 #
-#   distances = pairwise_distances(v0.reshape(1, D), embedding, metric=metric).reshape(V)
-#   # find the word index with the smallest distance
-#   idxs = distances.argsort()[:4]
-#   for idx in idxs:
-#     word = idx2word[idx]
+#   min_dist = float('inf')
+#   best_word = ''
+#   for word, v1 in iteritems(word2vec):
 #     if word not in (w1, w2, w3):
-#       best_word = word
-#       break
-#
+#       d = dist(v0, v1)
+#       if d < min_dist:
+#         min_dist = d
+#         best_word = word
 #   print(w1, "-", w2, "=", best_word, "-", w3)
+
+
+## faster
+def find_analogies(w1, w2, w3):
+  for w in (w1, w2, w3):
+    if w not in word2vec:
+      print("%s not in dictionary" % w)
+      return
+
+  king = word2vec[w1]
+  man = word2vec[w2]
+  woman = word2vec[w3]
+  v0 = king - man + woman
+
+  distances = pairwise_distances(v0.reshape(1, D), embedding, metric=metric).reshape(V)
+  # find the word index with the smallest distance
+  idxs = distances.argsort()[:4]
+  for idx in idxs:
+    word = idx2word[idx]
+    if word not in (w1, w2, w3):
+      best_word = word
+      break
+
+  print(w1, "-", w2, "=", best_word, "-", w3)
 
 
 def nearest_neighbors(w, n=5):
